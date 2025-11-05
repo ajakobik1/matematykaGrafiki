6cbdbb2125703f9b25f2cbc940f727f944699d6d
251532 <Olciak5920048753@>
2025-11-05 11:09:51 +0100

    poprawka

--- zad2.cpp
+++ zad2.cpp
@@ -120,6 +120,64 @@ public:
         return R;
     }
 
+    double Det3x3(double a1, double a2, double a3,
+              double b1, double b2, double b3,
+              double c1, double c2, double c3) const
+    {
+        return a1 * (b2 * c3 - b3 * c2)
+             - a2 * (b1 * c3 - b3 * c1)
+             + a3 * (b1 * c2 - b2 * c1);
+    }
+
+    Macierz Odwrotna() const {
+        Macierz wynik;
+        double det = 0.0;
+
+        // obliczamy wyznacznik 4x4
+        det =
+            m[0][0] * Det3x3(m[1][1], m[1][2], m[1][3],
+                             m[2][1], m[2][2], m[2][3],
+                             m[3][1], m[3][2], m[3][3])
+          - m[0][1] * Det3x3(m[1][0], m[1][2], m[1][3],
+                             m[2][0], m[2][2], m[2][3],
+                             m[3][0], m[3][2], m[3][3])
+          + m[0][2] * Det3x3(m[1][0], m[1][1], m[1][3],
+                             m[2][0], m[2][1], m[2][3],
+                             m[3][0], m[3][1], m[3][3])
+          - m[0][3] * Det3x3(m[1][0], m[1][1], m[1][2],
+                             m[2][0], m[2][1], m[2][2],
+                             m[3][0], m[3][1], m[3][2]);
+
+        if (fabs(det) < 1e-9) {
+            std::cerr << "Macierz nie ma odwrotnosci (det=0)\n";
+            return wynik;
+        }
+
+        // Obliczamy macierz dopełnień algebraicznych
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                double sub[3][3];
+                int subi = 0;
+                for (int ii = 0; ii < 4; ++ii) {
+                    if (ii == i) continue;
+                    int subj = 0;
+                    for (int jj = 0; jj < 4; ++jj) {
+                        if (jj == j) continue;
+                        sub[subi][subj] = m[ii][jj];
+                        subj++;
+                    }
+                    subi++;
+                }
+                double subDet = Det3x3(sub[0][0], sub[0][1], sub[0][2],
+                                       sub[1][0], sub[1][1], sub[1][2],
+                                       sub[2][0], sub[2][1], sub[2][2]);
+                wynik.m[j][i] = ((i + j) % 2 == 0 ? 1 : -1) * subDet / det;
+            }
+        }
+        return wynik;
+    }
+
+
     // Wyswietlenie macierzy
     void Pokaz() const {
         std::cout << std::fixed << std::setprecision(2);
